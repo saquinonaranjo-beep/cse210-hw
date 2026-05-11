45c8a1c008cc3d82a6c6b5022a43d8a81a4e0cf3
seraquino <saquinonaranjo@byupathway.edu>
2026-05-11 11:58:14 -0600

    ecersi 5 done

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,3 +1,44 @@
 using System;
 
-Console.WriteLine("Hello World! This is the Exercise5 Project.");
+class Program
+{
+    static void Main(string[] args)
+    {
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int squared = SquareNumber(number);
+        DisplayResult(name, squared);
+    }   
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }  
+
+
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your favorite number? ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(string name, int squared)
+    {
+        Console.WriteLine($"{name}, the square is {squared}.");
+    }
+}
