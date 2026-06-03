002851bb3b2667800762f8839d6b6dfb34fef781
seraquino <saquinonaranjo@byupathway.edu>
2026-06-02 20:58:08 -0600

    nice

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,3 +1,19 @@
 using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        MathAssignment math = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "8-19");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
 
-Console.WriteLine("Hello World! This is the Homework Project.");
+        Console.WriteLine();
+
+        WritingAssignment writing = new WritingAssignment("Susan Elrod", "European History", "The Causes of World War II");
+        Console.WriteLine(writing.GetSummary()); 
+        Console.WriteLine(writing.GetWritingInformation());
+    }
+
+
+
+}
